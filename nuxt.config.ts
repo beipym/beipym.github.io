23502a098f637abc9aa4250e9b8dc54f1658dc71
beipym <payamb.wrk@gmail.com>
2023-12-20 16:08:41 +0330

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,12 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/beipym.github.io/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
