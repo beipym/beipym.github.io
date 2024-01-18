87b0a25497a65d0c50e7df58638dd0230b2a9882
beipym <payamb.wrk@gmail.com>
2024-01-18 20:57:22 +0330

    add a default layout

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<script>
+    
+</script>
+
+<template>
+    <div class="w-screen h-screen absolute top-0 overflow-x-hidden overflow-y ">
+        
+
+        <!-- <nav class="shadow-[2px_2px] shadow-lg shadow-nGreen text-white rounded-lg m-4 px-8 py-2 bg-nGray after:">
+            <span class="text-lg">
+                Home
+            </span>
+        </nav> -->
+            
+
+    
+      <slot />
+
+    </div>
+</template>
+
+<style scoped>
+ 
+</style>
