2d5d37fd3f96fcea6fb19c3c50b19f5fd80fb4aa
beipym <payamb.wrk@gmail.com>
2024-01-18 22:58:27 +0330

    update section one

--- components/sectionOne/SectionOneBase.vue
+++ components/sectionOne/SectionOneBase.vue
@@ -1,35 +1,39 @@
 <script>
-
 </script>
 
 <template>
-    <div class="text-white flex flex-col ml-16 align-center justify-center w-full h-full">
-        <span class="mb-2">
-       
-        </span>
-        <span>
-            hi👋🏻 my name is
-        </span>
-        <div class="flex flex-row gap-2 align-center items-center mb-8">
-            <span class="font-bold text-6xl">
-                Payam
-            </span>
-        </div>
+    <div class="flex flex-row w-full h-full">
 
-        <div class="flex flex-row gap-1 items-center">
-            <span>
-                Im a     
+        <div class="text-white flex flex-col ml-16 align-center justify-center w-full h-full">
+            <span class="mb-2">
+        
             </span>
             <span>
-                Developer
+                hi👋🏻 my name is
             </span>
-            <span>
-                who is trying to stay
-            </span>
-            <span class="text-4xl">
-                CrÉ@tIf
-            </span>
+            <div class="flex flex-row gap-2 align-center items-center mb-8">
+                <span class="font-bold text-6xl">
+                    Payam
+                </span>
+            </div>
+
+            <div class="flex flex-row gap-1 items-center">
+                <span>
+                    Im a     
+                </span>
+                <span>
+                    Developer
+                </span>
+                <span>
+                    who is trying to stay
+                </span>
+                <span class="text-4xl">
+                    CrÉ@tIf
+                </span>
+            </div>
         </div>
+        
+
     </div>
 </template>
 
